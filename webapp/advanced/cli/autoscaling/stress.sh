b94fb68e449edc67e9c46931db9f2b230eb9c006
Twoje imię i nazwsko <djkormo@gmail.com>
2019-04-05 00:20:40 +0200

    adding stress.sh

--- webapp/advanced/cli/autoscaling/stress.sh
+++ webapp/advanced/cli/autoscaling/stress.sh
@@ -3,3 +3,17 @@ apt-get install stress-ng -y
 
 stress-ng --cpu 4 --io 2 --vm 1 --vm-bytes 256M --timeout 60s
 
+
+
+
+
+
+#!/bin/bash
+while :
+do
+        echo "Press [CTRL+C] to stop.."
+        ab  -t 60 -n 10000000 https://mywebapp2019fordemoauto.azurewebsites.net/
+        sleep 1
+done
+
+
