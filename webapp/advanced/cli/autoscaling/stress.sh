e3f578d46bf9ad4bd87258faa01222e545b24b4d
Twoje imię i nazwsko <djkormo@gmail.com>
2019-04-08 21:47:13 +0200

    adding pi.php

--- webapp/advanced/cli/autoscaling/stress.sh
+++ webapp/advanced/cli/autoscaling/stress.sh
@@ -12,11 +12,15 @@ stress-ng --cpu 4 --io 2 --vm 1 --vm-bytes 256M --timeout 60s
 while :
 do
         echo "Press [CTRL+C] to stop.."
-        ab  -t 60 -n 10000000 https://azwebapp-2019-autoscale21609.azurewebsites.net/
+        ab  -t 60 -n 10000000 https://azwebapp-2019-autoscale22825.azurewebsites.net/
         sleep 1
 done
 
 # using Curl from Azure Cloud Shell 
 
-for i in {0..600}; do (curl -Is https://azwebapp-2019-autoscale21609.azurewebsites.net/pi.php | head -n1 &) 2>/dev/null; sleep 1; done;
+for i in {0..600}; do (curl -Is http://azwebapp-2019-slot1832.azurewebsites.net/www/pi.php | head -n1 &) 2>/dev/null; sleep 1; done;
 
+https://azwebapp-2019-autoscale14160.azurewebsites.net/
+
+
+./letsencrypt-auto --apache -d djkormo.ml -d www.djkormo.ml
